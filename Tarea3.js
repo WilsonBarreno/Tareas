75ca61c09f161e6e639dc6c5e7e0125c839251c6
Wilson Barreno <wilson.barreno@galileo.edu>
2018-02-12 21:49:13 -0600

    Commit de tarea3

---
+++ Tarea3.js
@@ -0,0 +1,63 @@
+var Carnet=11104051;
+var Mes=12;
+switch(Mes)
+{
+  case 1: alert("Es invierno")
+  break;
+  case 2: alert("Es invierno")
+  break;
+  case 3: alert ("Es primavera")
+  break;
+  case 4: alert ("Es primavera")
+  break;
+  case 5: alert ("Es primavera")
+  break;
+  case 6: alert ("Es verano")
+  break;
+  case 7: alert ("Es verano")
+  break;
+  case 8: alert ("Es verano")
+  break;
+  case 9: alert ("Es otoño")
+  break;
+  case 10: alert ("Es otoño")
+  break;
+  case 11: alert ("Es otoño")
+  break;
+  case 12: alert ("Es invierno")
+  break;
+}
+
+alert ("Mi carnet es: "+Carnet +" "+"Mi Nombre es: Wilson Fernando Barreno Ola")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
